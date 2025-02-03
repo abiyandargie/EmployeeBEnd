c9a999a1f2dacd3350558ac6c9e8dba6da676c95
abiyandargie <andargieabiy85@gmail.com>
2025-02-03 23:35:03 +0300

    updated index.js

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
-import User from "../models/User.js";
+import User from "../models/Users.js";
 import dotenv from "dotenv";
 
 dotenv.config();
